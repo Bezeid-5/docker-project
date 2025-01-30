99d499de488bafd260035ee3e9bdf36dc2eaaa7a
zeini <23025@supnum.mr>
2025-01-30 00:19:50 +0000

    rade wvate

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "welcom to index page"
+def i():
+    return render_template('index.html')
 
-@app.route('/ipa')
-def iap():
-    return jsonify({"message": "Bienvenue dans mon application Flask!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
